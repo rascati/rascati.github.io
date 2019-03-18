3c74622319e1d9c7720ee9729e9e4ea3e5c3b660
Michael Rascati <rascatimichael@gmail.com>
2019-03-18 15:39:45 -0400

    refactored index by removing the main layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,67 +1,70 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from '../components/seo'
 
-import MainGrid from "../components/MainGrid"
-import PieceGrid from "../components/PieceGrid"
-import Piece from "../components/Piece"
-import Pill from "../components/Pill"
+import MainGrid from '../components/MainGrid'
+import PieceGrid from '../components/PieceGrid'
+import Piece from '../components/Piece'
+import Pill from '../components/Pill'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Michael Rascati" keywords={[`michael`, `rascati`, `web`, `developer`]} />
+  <div class="background">
+    <SEO
+      title="Michael Rascati"
+      keywords={[`michael`, `rascati`, `web`, `developer`]}
+    />
 
     <MainGrid />
 
     <PieceGrid>
-
-      <Piece 
+      <Piece
         title="Letterforms"
         description="A study in typographic geometry combining glyphs and creating novel shapes"
-        link="../work/letterforms">
+        link="../work/letterforms"
+      >
         <Pill color="SkyBlue">Design</Pill>
       </Piece>
 
-      <Piece 
+      <Piece
         title="FedEx Icon System"
         description="A responsive SVG icon system for FedEx"
-        link="../work/fedex">
+        link="../work/fedex"
+      >
         <Pill color="LightCoral">Development</Pill>
       </Piece>
 
-      <Piece 
+      <Piece
         title="J!Mate Jeopardy Companion App"
         description="A companion app for Jeopardy targeted at elders"
-        link="../work/jmate">
+        link="../work/jmate"
+      >
         <Pill color="SkyBlue">Design</Pill>
         <Pill color="MediumSeaGreen">UX</Pill>
       </Piece>
 
-      <Piece 
+      <Piece
         title="Planet Queen"
         description="A JSON-driven wildlife conservation website"
-        link="../work/planet-queen">
+        link="../work/planet-queen"
+      >
         <Pill color="LightCoral">Development</Pill>
       </Piece>
 
-      <Piece 
+      <Piece
         title="TEDxCambridge"
         description="An animated presentation page for one of the largest TEDx events in the country"
-        link="../work/tedx">
+        link="../work/tedx"
+      >
         <Pill color="LightCoral">Development</Pill>
       </Piece>
 
-      <Piece 
+      <Piece
         title="Gorgiality"
         description="A showcase website for a photography exhibition and book sale by Erez Sabag"
-        link="../work/gorgiality">
+        link="../work/gorgiality"
+      >
         <Pill color="LightCoral">Development</Pill>
       </Piece>
-
-      
-
-
 
       {/* <Piece 
         title="Venmo Case Study"
@@ -78,10 +81,8 @@ const IndexPage = () => (
         link="../work/nupt">
         <Pill color="SkyBlue">Design</Pill>
       </Piece> */}
-
     </PieceGrid>
-
-  </Layout>
+  </div>
 )
 
 export default IndexPage
