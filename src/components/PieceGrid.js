d52ac5fc83d2d213786923bbbe357beb01562c8a
Michael Rascati <rascatimichael@gmail.com>
2019-03-18 15:49:34 -0400

    refactored piecegrid to be an explicitly defined layout like maingrid

--- src/components/PieceGrid.js
+++ src/components/PieceGrid.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
+
+import Piece from '../components/Piece'
+import Pill from '../components/Pill'
 
 const GridWrapper = styled.div`
   display: grid;
@@ -25,6 +28,79 @@ const GridWrapper = styled.div`
   }
 `
 
-const PieceGrid = props => <GridWrapper>{props.children}</GridWrapper>
+// const PieceGrid = props => <GridWrapper>{props.children}</GridWrapper>
+
+class PieceGrid extends Component {
+  render() {
+    return (
+      <GridWrapper>
+        <Piece
+          title="Letterforms"
+          description="A study in typographic geometry combining glyphs and creating novel shapes"
+          link="../work/letterforms"
+        >
+          <Pill color="SkyBlue">Design</Pill>
+        </Piece>
+
+        <Piece
+          title="FedEx Icon System"
+          description="A responsive SVG icon system for FedEx"
+          link="../work/fedex"
+        >
+          <Pill color="LightCoral">Development</Pill>
+        </Piece>
+
+        <Piece
+          title="J!Mate Jeopardy Companion App"
+          description="A companion app for Jeopardy targeted at elders"
+          link="../work/jmate"
+        >
+          <Pill color="SkyBlue">Design</Pill>
+          <Pill color="MediumSeaGreen">UX</Pill>
+        </Piece>
+
+        <Piece
+          title="Planet Queen"
+          description="A JSON-driven wildlife conservation website"
+          link="../work/planet-queen"
+        >
+          <Pill color="LightCoral">Development</Pill>
+        </Piece>
+
+        <Piece
+          title="TEDxCambridge"
+          description="An animated presentation page for one of the largest TEDx events in the country"
+          link="../work/tedx"
+        >
+          <Pill color="LightCoral">Development</Pill>
+        </Piece>
+
+        <Piece
+          title="Gorgiality"
+          description="A showcase website for a photography exhibition and book sale by Erez Sabag"
+          link="../work/gorgiality"
+        >
+          <Pill color="LightCoral">Development</Pill>
+        </Piece>
+
+        {/* <Piece 
+        title="Venmo Case Study"
+        description="Looking at improving the main workflows of the Venmo Android app - find a problematic mobile interface and
+        redesign it to improve the user experience."
+        link="">
+        <Pill color="SkyBlue">Design</Pill>
+        <Pill color="MediumSeaGreen">UX</Pill>
+      </Piece> */}
+
+        {/* <Piece 
+        title="NUPT Logos"
+        description="A new logo system for Northeastern's Personal Training department"
+        link="../work/nupt">
+        <Pill color="SkyBlue">Design</Pill>
+      </Piece> */}
+      </GridWrapper>
+    )
+  }
+}
 
 export default PieceGrid
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import SEO from '../components/seo'
 
 import MainGrid from '../components/MainGrid'
 import PieceGrid from '../components/PieceGrid'
-import Piece from '../components/Piece'
-import Pill from '../components/Pill'
 
 const IndexPage = () => (
   <div class="background">
@@ -16,72 +14,7 @@ const IndexPage = () => (
 
     <MainGrid />
 
-    <PieceGrid>
-      <Piece
-        title="Letterforms"
-        description="A study in typographic geometry combining glyphs and creating novel shapes"
-        link="../work/letterforms"
-      >
-        <Pill color="SkyBlue">Design</Pill>
-      </Piece>
-
-      <Piece
-        title="FedEx Icon System"
-        description="A responsive SVG icon system for FedEx"
-        link="../work/fedex"
-      >
-        <Pill color="LightCoral">Development</Pill>
-      </Piece>
-
-      <Piece
-        title="J!Mate Jeopardy Companion App"
-        description="A companion app for Jeopardy targeted at elders"
-        link="../work/jmate"
-      >
-        <Pill color="SkyBlue">Design</Pill>
-        <Pill color="MediumSeaGreen">UX</Pill>
-      </Piece>
-
-      <Piece
-        title="Planet Queen"
-        description="A JSON-driven wildlife conservation website"
-        link="../work/planet-queen"
-      >
-        <Pill color="LightCoral">Development</Pill>
-      </Piece>
-
-      <Piece
-        title="TEDxCambridge"
-        description="An animated presentation page for one of the largest TEDx events in the country"
-        link="../work/tedx"
-      >
-        <Pill color="LightCoral">Development</Pill>
-      </Piece>
-
-      <Piece
-        title="Gorgiality"
-        description="A showcase website for a photography exhibition and book sale by Erez Sabag"
-        link="../work/gorgiality"
-      >
-        <Pill color="LightCoral">Development</Pill>
-      </Piece>
-
-      {/* <Piece 
-        title="Venmo Case Study"
-        description="Looking at improving the main workflows of the Venmo Android app - find a problematic mobile interface and
-        redesign it to improve the user experience."
-        link="">
-        <Pill color="SkyBlue">Design</Pill>
-        <Pill color="MediumSeaGreen">UX</Pill>
-      </Piece> */}
-
-      {/* <Piece 
-        title="NUPT Logos"
-        description="A new logo system for Northeastern's Personal Training department"
-        link="../work/nupt">
-        <Pill color="SkyBlue">Design</Pill>
-      </Piece> */}
-    </PieceGrid>
+    <PieceGrid />
   </div>
 )
 
