b448001eeba8148e4891207c5b74c262da2110fd
Michael Rascati <rascatimichael@gmail.com>
2019-03-20 12:47:26 -0400

    adjust project grid layout

--- src/components/PieceGrid.js
+++ src/components/PieceGrid.js
@@ -9,6 +9,7 @@ const GridWrapper = styled.div`
   margin: 1.5em;
   margin-top: 0;
   border-left: 2px solid #0c0c0c;
+  grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
 
   @media only screen and (max-width: 750px) {
     display: flex;
@@ -21,6 +22,13 @@ class PieceGrid extends Component {
     return (
       <GridWrapper>
         <Piece
+          title="FedEx Icon System"
+          description="A responsive SVG icon system for FedEx"
+          link="../work/fedex"
+          category="Development"
+        />
+
+        <Piece
           title="Letterforms"
           description="A study in typographic geometry combining glyphs and creating novel shapes"
           link="../work/letterforms"
@@ -28,9 +36,9 @@ class PieceGrid extends Component {
         />
 
         <Piece
-          title="FedEx Icon System"
-          description="A responsive SVG icon system for FedEx"
-          link="../work/fedex"
+          title="Planet Queen"
+          description="A JSON-driven wildlife conservation website"
+          link="../work/planet-queen"
           category="Development"
         />
 
@@ -39,13 +47,6 @@ class PieceGrid extends Component {
           description="A companion app for Jeopardy targeted at elders"
           link="../work/jmate"
           category="Design, UX"
-        />
-
-        <Piece
-          title="Planet Queen"
-          description="A JSON-driven wildlife conservation website"
-          link="../work/planet-queen"
-          category="Development"
         />
 
         <Piece
